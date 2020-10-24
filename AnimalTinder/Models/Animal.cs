f764a6fd61e1ad25b0d124d8d9a92821d04bbd9f
Jana Gjelevska <jana.gelevska@hotmail.com>
2020-10-24 19:48:48 +0200

    AnimalTinder first version

---
+++ AnimalTinder/Models/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnimalTinder.Models
+{
+    public class Animal
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ImgURL { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AnimalTinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnimalTinder.Startup))]
+namespace AnimalTinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
